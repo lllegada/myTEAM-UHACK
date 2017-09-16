2b767bb86124f1ec0347451e7af1022e1272985e
gregg <gmicay123@gmail.com>
2017-09-16 10:51:10 +0800

    added home button

--- uhack/resources/views/layouts/app.blade.php
+++ uhack/resources/views/layouts/app.blade.php
@@ -53,11 +53,12 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" id="profname" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                    <li><a href="/home">Home</a></li>
                                     <li><a href="{{ route('transact.index') }}">Transactions</a></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
